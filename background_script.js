cf6eb59088ac9c0a856cf0790ab22105a1d58ce1
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2021-02-19 19:31:34 +0100

    :sparkles: add basic support for Bible queries

--- background_script.js
+++ background_script.js
@@ -1,1 +1,44 @@
-// Put all the javascript code here, that you want to execute in background.
+const parseBibleQuery = (query) => {
+  const [book, chapter, verses] = (query.match(/((?:[1-3]\s?)?[A-Za-z]+) (\d+)(?:[,:]\s?(\d+(?:-\d+)?))?/) || []).slice(1);
+  const bookCapitalized = book[0].toUpperCase() + book.slice(1);
+
+  return {
+    book: bookCapitalized,
+    chapter,
+    verses,
+  };
+};
+
+const pismoSwietePl = ({ book, chapter, verses }) => {
+  const chapterUrl = `https://pismoswiete.pl/app/#/tome/${book}/chapter/${chapter}/`;
+  if (!verses) {
+    return chapterUrl;
+  }
+
+  return `${chapterUrl}${verses}?isVerseQuery=true`;
+};
+
+const openPage = (disposition, url) => {
+  switch (disposition) {
+    case 'currentTab':
+      browser.tabs.update({ url });
+      break;
+    case 'newForegroundTab':
+      browser.tabs.create({ url });
+      break;
+    case 'newBackgroundTab':
+      browser.tabs.create({ url, active: false });
+      break;
+  }
+};
+
+browser.omnibox.onInputEntered.addListener((query, disposition) => {
+  const { book, chapter, verses } = parseBibleQuery(query);
+  if (!book && !chapter) {
+    return;
+  }
+
+  const url = pismoSwietePl({ book, chapter, verses });
+  openPage(disposition, url);
+});
+
