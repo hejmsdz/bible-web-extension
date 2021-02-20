89c0d833c33b0ed1a15558c8ed0530a44b261871
Mikołaj Rozwadowski <hejmsdz@users.noreply.github.com>
2021-02-20 16:50:56 +0100

    :sparkles: add search suggestions

--- background_script.js
+++ background_script.js
@@ -1,3 +1,78 @@
+const BOOKS = {
+  'Rdz': 'Księga Rodzaju',
+  'Wj': 'Księga Wyjścia',
+  'Kpł': 'Księga Kapłańska',
+  'Lb': 'Księga Liczb',
+  'Pwt': 'Księga Powtórzonego Prawa',
+  'Joz': 'Księga Jozuego',
+  'Sdz': 'Księga Sędziów',
+  'Rt': 'Księga Rut',
+  '1Sm': 'Pierwsza Księga Samuela',
+  '2Sm': 'Druga Księga Samuela',
+  '1Krl': 'Pierwsza Księga Królewska',
+  '2Krl': 'Druga Księga Królewska',
+  '1Krn': 'Pierwsza Księga Kronik',
+  '2Krn': 'Druga Księga Kronik',
+  'Ezd': 'Księga Ezdrasza',
+  'Ne': 'Księga Nehemiasza',
+  'Tb': 'Księga Tobiasza',
+  'Jdt': 'Księga Judyty',
+  'Est': 'Księga Estery',
+  '1Mch': 'Pierwsza Księga Machabejska',
+  '2Mch': 'Druga Księga Machabejska',
+  'Hi': 'Księga Hioba',
+  'Ps': 'Księga Psalmów',
+  'Prz': 'Księga Przysłów',
+  'Koh': 'Księga Koheleta',
+  'Pnp': 'Pieśn nad Pieśniami',
+  'Mdr': 'Księga Mądrości',
+  'Syr': 'Księga Syracydesa',
+  'Iz': 'Księga Izajasza',
+  'Jr': 'Księga Jeremiasza',
+  'Lm': 'Księga Lamentacji',
+  'Ba': 'Księga Barucha',
+  'Ez': 'Księga Ezechiela',
+  'Dn': 'Księga Daniela',
+  'Oz': 'Księga Ozeasza',
+  'Jl': 'Księga Joela',
+  'Am': 'Księga Amosa',
+  'Ab': 'Księga Abidiasza',
+  'Jon': 'Księga Jonasza',
+  'Mi': 'Księga Micheasza',
+  'Na': 'Księga Nahuma',
+  'Ha': 'Księga Habakuka',
+  'So': 'Księga Sofoniasza',
+  'Ag': 'Księga Aggeusza',
+  'Za': 'Księga Zachariasza',
+  'Ml': 'Księga Malachiasza',
+
+  'Mt': 'Ewangelia wg św. Mateusza',
+  'Mk': 'Ewangelia wg św. Marka',
+  'Łk': 'Ewangelia wg św. Łukasza',
+  'J': 'Ewangelia wg św. Jana',
+  'Rz': 'List do Rzymian',
+  '1Kor': 'Pierwszy List do Koryntian',
+  '2Kor': 'Drugi List do Koryntian',
+  'Ga': 'List do Galatów',
+  'Ef': 'List do Efezjan',
+  'Flp': 'List do Filipian',
+  'Kol': 'List do Kolosan',
+  '1Tes': 'Pierwszy List do Tesaloniczan',
+  '2Tes': 'Drugi List do Tesaloniczan',
+  '1Tm': 'Pierwszy List do Tymoteusza',
+  '2Tm': 'Drugi List do Tymoteusza',
+  'Tt': 'List do Tytusa',
+  'Flm': 'List do Filemona',
+  'Hbr': 'List do Hebrajczyków',
+  'Jk': 'List św. Jakuba',
+  '1P': 'Pierwszy List św. Piotra',
+  '2P': 'Drugi List św. Piotra',
+  '1J': 'Pierwszy List św. Jana',
+  '2J': 'Drugi List św. Jana',
+  '3J': 'Trzeci List św. Jana',
+  'Ap': 'Apokalipsa św. Jana',
+};
+
 const formatBook = (book) => {
   return book
     .replace(' ', '')
@@ -5,10 +80,10 @@ const formatBook = (book) => {
 };
 
 const parseBibleQuery = (query) => {
-  const [book, chapter, verses] = (query.match(/((?:[1-3]\s?)?[A-Za-z]+) (\d+)(?:[,:]\s?(\d+(?:-\d+)?))?/) || []).slice(1);
+  const [book, chapter, verses] = (query.match(/((?:[1-3]\s?)?[A-Za-z]+)(?:\s)?(\d+)?(?:[,:]\s?(\d+(?:-\d+)?))?/) || []).slice(1);
 
   return {
-    book: formatBook(book),
+    book: book && formatBook(book),
     chapter,
     verses,
   };
@@ -36,6 +111,32 @@ const openPage = (disposition, url) => {
       break;
   }
 };
+
+browser.omnibox.setDefaultSuggestion({ description: 'Pismo Święte' });
+
+browser.omnibox.onInputChanged.addListener((query, suggest) => {
+  const { book, chapter, verses } = parseBibleQuery(query);
+
+  let suggestions;
+  if (!chapter && !query.endsWith(' ')) {
+    suggestions = Object.entries(BOOKS)
+      .filter(([symbol, name]) => symbol.startsWith(book))
+      .map(([symbol, name]) => ({ content: `${symbol} `, description: name }));
+  } else if (!verses) {
+    if (chapter) {
+      suggestions = [{ content: `${book} ${chapter}, `, description: `${BOOKS[book]}, rozdział ${chapter}` }];
+    } else {
+      suggestions = [{ content: `${book} `, description: `${BOOKS[book]}` }];
+    }
+  } else {
+    if (verses.includes('-')) {
+      suggestions = [{ content: `${book} ${chapter}, ${verses}`, description: `${BOOKS[book]}, rozdział ${chapter}, wersety ${verses}` }];
+    } else {
+      suggestions = [{ content: `${book} ${chapter}, ${verses}`, description: `${BOOKS[book]}, rozdział ${chapter}, werset ${verses}` }];
+    }
+  }
+  suggest(suggestions.sort((a, b) => a.content.localeCompare(b.content)));
+});
 
 browser.omnibox.onInputEntered.addListener((query, disposition) => {
   const { book, chapter, verses } = parseBibleQuery(query);
